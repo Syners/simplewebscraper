1c29d75b0799155ca9e6e3606650652696911fa6
Alex <alexander.ward1@gmail.com>
2016-02-03 15:20:00 -0600

    added chrome cookie import

--- build/lib/webscraper/exceptions.py
+++
@@ -1,9 +0,0 @@
-
-
-class Exceptions(object):
-    pass
-
-
-class SomeException(Exceptions):
-    def __init__(self):
-        Exceptions.__init__(self)
--- build/lib/webscraper/httpmethods.py
+++
@@ -1,8 +0,0 @@
-class Methods(object):
-    GET = 1
-    POST = 2
-    PUT = 3
-    DELETE = 4
-    HEAD = 5
-    OPTIONS = 6
-    CONNECT = 7
