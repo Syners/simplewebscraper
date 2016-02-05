d812642d00ffa1d14fdcb9129ae8660aab1ee2cb
Alex <alexander.ward1@gmail.com>
2016-02-05 11:09:59 -0600

    updated docs

--- examples/test.py
+++ examples/test.py
@@ -1,12 +1,46 @@
-from simplewebscraper import Browser, HTTPMethod, Scraper
+from simplewebscraper import Browser, HTTPMethod, Scraper, ProxyPool
 
 if __name__ == "__main__":
-    my_scraper = Scraper()
-    my_scraper.HTTP_mode = HTTPMethod.GET
-    # test.use_per_proxy_count = 5
-    # my_scraper.proxy_pool = ProxyPool.Hidester #{"https": ["https://212.119.246.138:8080"],"http": []}
-    my_scraper.cookies = Browser.Chrome  # Chrome or Firefox
-    # my_scraper.url = "https://myip.dnsdynamic.org"
-    my_scraper.url = "http://cfb.blob.core.windows.net/conceptfeedback/concepts/fullsize/52ea9738-22b4-47cc-8159-78dbf317e43d.jpg"
-    # my_scraper.download_path = "C:\\"
-    my_scraper.fetch()
+
+    example_GET = True
+    example_GET_parameters = True
+    example_POST = False
+    example_Proxy = False
+    example_cookie_import = False
+
+    if example_GET:
+        my_scraper = Scraper()
+        my_scraper.HTTP_mode = HTTPMethod.GET
+        my_scraper.url = "https://myip.dnsdynamic.org"
+        print my_scraper.fetch()
+
+    if example_GET_parameters:
+        my_scraper = Scraper()
+        my_scraper.HTTP_mode = HTTPMethod.GET
+        my_scraper.parameters = {'InData': "75791",
+                                 "submit": "Search"}
+        my_scraper.url = "http://www.melissadata.com/lookups/GeoCoder.asp"
+        print my_scraper.fetch()
+
+    if example_POST:
+        my_scraper = Scraper()
+        my_scraper.HTTP_mode = HTTPMethod.POST
+        my_scraper.parameters = {"email": "example@gmail.com",
+                                 "pass": "samplepassword"}
+        my_scraper.url = "https://www.dnsdynamic.org/auth.php"
+        print my_scraper.fetch()
+
+    if example_Proxy:
+        my_scraper = Scraper()
+        my_scraper.HTTP_mode = HTTPMethod.GET
+        my_scraper.use_per_proxy_count = 5
+        my_scraper.proxy_pool = ProxyPool.Hidester  #You can provide a group of proxies like this as well {"https": ["https://212.119.246.138:8080"],"http": []}
+        my_scraper.url = "https://myip.dnsdynamic.org"
+        print my_scraper.fetch()
+
+    if example_cookie_import:
+        my_scraper = Scraper()
+        my_scraper.HTTP_mode = HTTPMethod.GET
+        my_scraper.cookies = Browser.Chrome  # Chrome or Firefox
+        my_scraper.url = "https://myip.dnsdynamic.org"
+        print my_scraper.fetch()
